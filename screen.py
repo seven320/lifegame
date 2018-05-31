e1e298629eeac4798bbd8c44fafb0c61eccb176a
seven320 <yosyuaomenw@yahoo.co.jp>
2018-05-31 14:06:55 +0900

    一応完成

--- screen.py
+++ screen.py
@@ -2,19 +2,69 @@ import pygame
 from pygame.locals import *
 import numpy as np
 import sys
+#
+# X=600
+# Y=600
+# cell_size=15
+# pygame.init()
+# screen=pygame.display.set_mode((X,Y))
+# pygame.display.set_caption("LIFE GAME")
+# while(1):
+#     screen.fill((0,0,0))
+#     A=np.zeros((int(X/cell_size),int(Y/cell_size)))
+#     pygame.draw.rect(screen,(0,80,0),Rect(10,10,20,20))
+#     pygame.display.update()
+#     for event in pygame.event.get():
+#         if event.type == QUIT:                              # 閉じるボタンが押されたら終了
+#             pygame.quit()                                   # Pygameの終了(画面閉じられる)
+#             sys.exit()
 
-X=600
-Y=600
-cell_size=15
-pygame.init()
-screen=pygame.display.set_mode((X,Y))
-pygame.display.set_caption("LIFE GAME")
-while(1):
-    screen.fill((0,0,0))
-    A=np.zeros((int(X/cell_size),int(Y/cell_size)))
-    pygame.draw.rect(screen,(0,80,0),Rect(10,10,20,20))
-    pygame.display.update()
-    for event in pygame.event.get():
-        if event.type == QUIT:                              # 閉じるボタンが押されたら終了
-            pygame.quit()                                   # Pygameの終了(画面閉じられる)
-            sys.exit()
+def main():
+    (w,h) = (400,400)   # 画面サイズ
+    (x,y) = (w/2, h/2)
+    pygame.init()       # pygame初期化
+    pygame.display.set_mode((w, h), 0, 32)  # 画面設定
+    screen = pygame.display.get_surface()
+
+    while (1):
+        pygame.display.update()     # 画面更新
+        pygame.time.wait(30)        # 更新時間間隔
+        screen.fill((0, 20, 0, 0))  # 画面の背景色
+        # 円の中心座標が画面の範囲外にある場合
+        if x < 0:
+            x = 0
+        if x > w:
+            x = w
+        if y < 0:
+            y = 0
+        if y > h:
+            y = h
+        # 円を描画
+        pygame.draw.circle(screen, (0, 200, 0), (int(x), int(y)), 5)
+        # イベント処理
+        for event in pygame.event.get():
+            # 画面の閉じるボタンを押したとき
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            # キーを押したとき
+            if event.type == KEYDOWN:
+                # ESCキーなら終了
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                # 矢印キーなら円の中心座標を矢印の方向に移動
+                if event.key == K_LEFT:
+                    x -= 1
+                if event.key == K_RIGHT:
+                    x += 1
+                if event.key == K_UP:
+                    y -= 1
+                if event.key == K_DOWN:
+                    y += 1
+                if event.key == K_SPACE:
+                    x -= 1
+
+
+if __name__ == "__main__":
+    main()
--- test_pygame.py
+++ test_pygame.py
@@ -6,37 +6,6 @@ import sys,os
 import numpy as np
 import time
 
-# def main_3():
-#     pygame.init()
-#     screen=pygame.display.set_mode((400,300))
-#     pygame.display.set_caption("Test")#title bar
-#
-#     while(1):
-#         screen.fill((0,0,0))
-#         pygame.display.update()
-#         #イベント処理
-#         for event in pygame.event.get():
-#             if event.type==QUIT:
-#                 pygame.quit()
-#                 sys.exit()
-#
-# def main_2():
-#     pygame.init()
-#     screen=pygame.display.set_mode((400,300))
-#     pygame.display.set_caption("GAME")
-#
-#     while(1):
-#         screen.fill((0,0,0))
-#         pygame.draw.line(screen,(0,95,0), (100,0), (100,300), 1)
-#         pygame.draw.rect(screen,(0,80,0),Rect(10,10,80,50),5)
-#         pygame.draw.rect(screen,(0,80,0),Rect(20,20,30,40))
-#         pygame.display.update()
-#         #イベント処理
-#         for event in pygame.event.get():
-#             if event.type==QUIT:
-#                 pygame.quit()
-#                 sys.exit()
-
 def cell_update(x,y,a):
     count=0
     status=0
@@ -66,8 +35,6 @@ def cell_update(x,y,a):
                         count+=1
                 elif a[x+i][y+j]==1:
                     count+=1
-                    # print("該当カウント: x:{0} y:{1} i:{2} j:{3} count:{4}".format(x,y,i,j,count))
-    # print("x:{0},y:{1},count:{2}".format(x,y,count))
     if a[x][y]==0 and count==3:#誕生
         status=1
         # print("誕生{0}_{1}".format(x,y))
@@ -110,24 +77,41 @@ def load_life(life,A):
         print("error:入力が奇数個です。")
     return A
 
-
 def main():
+    state=False
     X=600
     Y=600
-    cell_size=10
+    cell_size=8
     pygame.init()
     screen=pygame.display.set_mode((X,Y))
     pygame.display.set_caption("LIFE GAME")
     A=np.zeros((int(X/cell_size),int(Y/cell_size)))
-    # printlife(A)
-    # b=status__update(A)
-    # printlife(b)
+
+    #life_game　図鑑　wikipedia参照　https://ja.wikipedia.org/wiki/%E3%83%A9%E3%82%A4%E3%83%95%E3%82%B2%E3%83%BC%E3%83%A0
     #グライダー銃
     life=[1,25,2,23,2,25,3,13,3,14,3,21,3,22,3,35,3,36,4,12,4,16,4,21,4,22,4,35,4,36,5,1,5,2,5,11,5,17,5,21,5,22,6,1,6,2,6,11
     ,6,15,6,17,6,18,6,23,6,25,7,11,7,17,7,25,8,12,8,16,9,13,9,14]
-    # life=[0,0,1,1,1,0,0,1]
+    #どんぐり
+    life_2=[41,42,42,44,43,41,43,42,43,45,43,46,43,47]
+    #ダイハード
+    life_3=[22,28,23,22,23,23,24,23,24,27,24,28,24,29]
+    #horizontal_line
+    life_4=[]
+    for i in range(Y//cell_size):
+        life_4.append((X//cell_size)//2)
+        life_4.append(i)
+    #vertical_line
+    life_5=[]
+    for i in range(X//cell_size):
+        life_5.append(i)
+        life_5.append((X//cell_size)//2)
+    #ブリーダー　直線型
+    # life_6=[2,2,3,2,4,2,5,2,6,2,7,2,8,2,9,2,11,2,12,2,13,2,14,2,15,2,19,2,20,2,21,2,28,2,29,2,30,2,31,2,32,2,33,2,34,2,35,2,37
+    # ,2,38,2,39,2,40,2,41,2]
+
     A=load_life(life,A)
     while(1):
+
         screen.fill((0,0,0))
         #vertical line
         for i in range(int(X/cell_size)):
@@ -135,19 +119,48 @@ def main():
         #horizontal line
         for j in range(int(Y/cell_size)):
             pygame.draw.line(screen,(0,95,0),(0,j*cell_size),(X,j*cell_size),1)
-        y,x=np.where(A==1)
-        for k in range(x.size):
-            # print(x[k],y[k])
-            pygame.draw.rect(screen,(0,95,0),(x[k]*cell_size,y[k]*cell_size,cell_size,cell_size))
-            #最初の２変数はx,y座標、後ろの２変数は長方形のサイズ
-        # printlife(A)
-        A=status__update(A)
-        # time.sleep(10)
-        pygame.display.update()
+
+
         for event in pygame.event.get():
+            #カーソル操作時
+            if event.type==MOUSEBUTTONDOWN and event.button==1:
+                x_pix,y_pix=event.pos
+                A[y_pix//cell_size][x_pix//cell_size]=not(A[y_pix//cell_size][x_pix//cell_size])
+            #終了用イベント
             if event.type==QUIT:
                 pygame.quit()
                 sys.exit()
+            #キー入力時
+            if event.type==pygame.KEYDOWN:
+                #終了機能
+                if event.key==K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                if event.key==K_BACKSPACE:
+                    A=np.zeros((int(X/cell_size),int(Y/cell_size)))
+                if event.key==K_1: load_life(life,A)
+                if event.key==K_2: load_life(life_2,A)
+                if event.key==K_3: load_life(life_3,A)
+                if event.key==K_4: load_life(life_4,A)
+                if event.key==K_5: load_life(life_5,A)
+                if event.key==K_6:pass
+                if event.key==K_7:pass
+                if event.key==K_8:pass
+                if event.key==K_9:pass
+                #pause機能
+                if event.key==pygame.K_SPACE: state=not(state)
+        #cell表示
+        x,y=np.where(A==1)
+        for k in range(x.size):
+            # print(x[k],y[k])
+            pygame.draw.rect(screen,(0,95,0),(y[k]*cell_size,x[k]*cell_size,cell_size,cell_size))
+            #最初の２変数はx,y座標、後ろの２変数は長方形のサイズ
+        pygame.display.update()
+        #running
+        if state==0: A=status__update(A)
+        #pause
+        elif state==1:pass
+
 
 if __name__=="__main__":
     main()
